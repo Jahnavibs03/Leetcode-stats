5cd2c3f998cb74726a02f4bd30c4fa2c5192d6f2
JAHNAVI B S <134682079+Jahnavibs03@users.noreply.github.com>
2025-06-23 14:21:39 +0530

    Time: 5 ms (26.32%) | Memory: 18.6 MB (35.66%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -3,21 +3,25 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
+        pnts = []
         m = len(matrix[0])
         n = len(matrix)
         for i in range(n):
             for j in range(m):
                 if matrix[i][j] == 0:
-                    for k in range(n):
-                        #print(k,j)
-                        if matrix[k][j] == 0:
-                            continue
-                        matrix[k][j] = -11
-                    for k in range(m):
-                        #print(i,k)
-                        if matrix[i][k] == 0:
-                            continue
-                        matrix[i][k] = -11 
+                    pnts.append((i,j))
+                    
+        for i,j in pnts:
+            for k in range(n):
+                #print(k,j)
+                if matrix[k][j] == 0:
+                    continue
+                matrix[k][j] = -11
+            for k in range(m):
+                #print(i,k)
+                if matrix[i][k] == 0:
+                    continue
+                matrix[i][k] = -11 
         for i in range(n):
             for j in range(m):
                 if matrix[i][j] == -11:
