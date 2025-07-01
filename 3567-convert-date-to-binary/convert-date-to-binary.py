fc654db8aeb6a63855ebcacc86c006c6c7d606d3
JAHNAVI B S <134682079+Jahnavibs03@users.noreply.github.com>
2025-07-01 15:13:02 +0530

    Time: 1 ms (18.52%) | Memory: 17.9 MB (30.01%) - LeetSync

--- 3567-convert-date-to-binary/convert-date-to-binary.py
+++ 3567-convert-date-to-binary/convert-date-to-binary.py
@@ -2,5 +2,5 @@ class Solution:
     def convertDateToBinary(self, date: str) -> str:
         arr = date.split("-")
         res = format(int(arr[0]),'b') + "-" + format(int(arr[1]),'b') + "-" + format(int(arr[2]),'b')
-        print(res)
+        #print(res)
         return res
